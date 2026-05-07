5e38897143dbcab020633f0162088f15fe4af877
RaniaAkramm <ssess535@gmail.com>
2026-05-07 22:57:22 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,9 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}", // هذا السطر يسمح بقراءة ملف page.js في الصفحة الرئيسية
+    "./lib/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 }
